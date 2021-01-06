1b19a89b0f03cfaaf553903619c82b89947d36b3
rcardin <riccardo.cardin@gmail.com>
2021-01-06 15:32:06 +0100

    Added cancellation to copy file routine

--- src/main/scala/in/rcard/tutorial/file/copy/CopyFileMain.scala
+++ src/main/scala/in/rcard/tutorial/file/copy/CopyFileMain.scala
@@ -1,30 +1,35 @@
 package in.rcard.tutorial.file.copy
 
-import cats.effect.{ExitCode, IO, IOApp, Resource}
+import cats.effect.concurrent.Semaphore
+import cats.effect.{Concurrent, ExitCode, IO, IOApp, Resource}
 import cats.implicits.catsSyntaxFlatMapOps
 
-import java.io.{File, FileInputStream, FileOutputStream, InputStream, OutputStream}
+import java.io._
 
 object CopyFileMain extends IOApp {
 
-  def inputStream(f: File): Resource[IO, FileInputStream] =
+  def inputStream(f: File, guard: Semaphore[IO]): Resource[IO, FileInputStream] =
     Resource.make {
       IO(new FileInputStream(f))
     } { inStream =>
-      IO(inStream.close()).handleErrorWith(_ => IO.unit)
+      guard.withPermit {
+        IO(inStream.close()).handleErrorWith(_ => IO.unit)
+      }
     }
 
-  def outputStream(f: File): Resource[IO, FileOutputStream] =
+  def outputStream(f: File, guard: Semaphore[IO]): Resource[IO, FileOutputStream] =
     Resource.make {
       IO(new FileOutputStream(f))
     } { outStream =>
-      IO(outStream.close()).handleErrorWith(_ => IO.unit)
+      guard.withPermit {
+        IO(outStream.close()).handleErrorWith(_ => IO.unit)
+      }
     }
 
-  def inputOutputStream(in: File, out: File): Resource[IO, (FileInputStream, FileOutputStream)] =
+  def inputOutputStream(in: File, out: File, guard: Semaphore[IO]): Resource[IO, (FileInputStream, FileOutputStream)] =
     for {
-      inStream <- inputStream(in)
-      outStream <- outputStream(out)
+      inStream <- inputStream(in, guard)
+      outStream <- outputStream(out, guard)
     } yield (inStream, outStream)
 
   def transmit(origin: InputStream, destination: OutputStream, buffer: Array[Byte], acc: Long): IO[Long] =
@@ -43,10 +48,26 @@ object CopyFileMain extends IOApp {
       total <- transmit(origin, destination, buffer, 0L)
     } yield total
 
-  def copy(origin: File, destination: File): IO[Long] =
-    inputOutputStream(origin, destination).use { case (in, out) =>
-      transfer(in, out)
-    }
+  def copy(origin: File, destination: File)(implicit concurrent: Concurrent[IO]): IO[Long] = {
+    for {
+      guard <- Semaphore[IO](1)
+      count <-
+        inputOutputStream(origin, destination, guard).use { case (in, out) =>
+          guard.withPermit(transfer(in, out))
+        }
+    } yield count
+  }
 
-  override def run(args: List[String]): IO[ExitCode] = ???
+  override def run(args: List[String]): IO[ExitCode] =
+    for {
+      _ <-
+        if (args.length < 2)
+          IO.raiseError(new IllegalArgumentException("Need origin and destination files"))
+        else
+          IO.unit
+      orig = new File(args.head)
+      dest = new File(args(1))
+      count <- copy(orig, dest)
+      _ <- IO(println(s"$count bytes copied from ${orig.getPath} to ${dest.getPath}"))
+    } yield ExitCode.Success
 }
